0bba2aed51ce0fbdc369e8eda6f2664ff9868e1a
Matthew Crawford <mcrawford0126@gmail.com>
2024-04-26 18:05:39 -0400

    video endpoint and cors

--- python/server.py
+++ python/server.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
+
 from blueprints.associations_controller.associations import bp as association_bp
 from blueprints.settings_controller.settings import bp as settings_bp
 
 app = Flask(__name__)
+CORS(app)
+
 app.register_blueprint(association_bp, url_prefix='/associations')
 app.register_blueprint(settings_bp, url_prefix='/settings')
 
