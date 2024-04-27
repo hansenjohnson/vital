d3b394bfddc86071aa326bb536f8135ff34c7c0f
Matthew Crawford <mcrawford0126@gmail.com>
2024-04-27 15:47:10 -0400

    remove video changes

--- python/server.py
+++ python/server.py
@@ -3,16 +3,13 @@ from flask_cors import CORS
 from blueprints.associations_controller.associations import bp as association_bp
 from blueprints.sightings_controller.sightings import bp as sightings_bp
 from blueprints.settings_controller.settings import bp as settings_bp
-from blueprints.video_controller.videos import bp as video_bp
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(association_bp, url_prefix='/associations')
 app.register_blueprint(sightings_bp, url_prefix='/sightings')
-
 app.register_blueprint(settings_bp, url_prefix='/settings')
-app.register_blueprint(video_bp, url_prefix='/videos')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- ui/containers/Tools.jsx
+++ ui/containers/Tools.jsx
@@ -9,11 +9,6 @@ import MainActionButton from '../components/MainActionButton'
 import ToolButton from '../components/ToolButton'
 import Sidebar from '../components/Sidebar'
 
-import Settings from '../api/settings'
-import SettingsKeys from '../constants/settingKeys'
-import SETTING_KEYS from "../constants/settingKeys";
-
-
 const ToolsContainer = ({ setRoute, setFolderOfVideosToCreate }) => {
   useEffect(() => {
     window.api.setTitle('Video Catalog Suite')
@@ -30,7 +25,6 @@ const ToolsContainer = ({ setRoute, setFolderOfVideosToCreate }) => {
     if (!folderPath) return
     setFolderOfVideosToCreate(folderPath)
     setRoute(ROUTES.ASSOCIATIONS_CREATE)
-    await Settings.save({'folder_of_videos_to_create': folderPath})
   }
 
   return (
