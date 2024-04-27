ef1c1c76b84d61bef940b313c37c7cb9b72517d2
Matthew Crawford <mcrawford0126@gmail.com>
2024-04-26 22:47:19 -0400

    add file_names endpoint

--- python/blueprints/videos_controller/videos.py
+++ python/blueprints/videos_controller/videos.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, send_from_directory
+import os
+
+from flask import Blueprint, send_from_directory, jsonify
 from settings.settings_service import SettingsService
 from settings.settings_enum import SettingsEnum
 
@@ -6,6 +8,14 @@ bp = Blueprint('videos', __name__)
 settings_service = SettingsService()
 
 
-@bp.route('/<path:filename>', methods=['GET'])
-def video(filename):
-    return send_from_directory(settings_service.get_setting(SettingsEnum.FOLDER_OF_VIDEOS.value), filename)
+@bp.route('/<path:foldername>/<path:filename>', methods=['GET'])
+def video(folder_name, filename):
+    file_path = str(os.path.join(folder_name, filename))
+    return send_from_directory(settings_service.get_setting(SettingsEnum.FOLDER_OF_VIDEOS.value), file_path)
+
+
+@bp.route('/file_names', methods=['GET'])
+def get_file_names():
+    folder_of_videos = settings_service.get_setting(SettingsEnum.FOLDER_OF_VIDEOS.value)
+    file_names = [f for f in os.listdir(folder_of_videos) if os.path.isfile(os.path.join(folder_of_videos, f))]
+    return jsonify(file_names)
--- python/server.py
+++ python/server.py
@@ -3,12 +3,14 @@ from flask_cors import CORS
 
 from blueprints.associations_controller.associations import bp as association_bp
 from blueprints.settings_controller.settings import bp as settings_bp
+from blueprints.videos_controller.videos import bp as videos_bp
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(association_bp, url_prefix='/associations')
 app.register_blueprint(settings_bp, url_prefix='/settings')
+app.register_blueprint(videos_bp, url_prefix='/videos')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
