5a4b52b6b009c2738241c05390af678fa7e7fce9
Ben Zenker <imbenzenker@gmail.com>
2024-05-07 21:56:34 -0600

    adjusts timeline right to account for scrollbar

--- ui/components/VideoTimeline.jsx
+++ ui/components/VideoTimeline.jsx
@@ -2,6 +2,9 @@ import { useState, useRef, useEffect, useCallback } from 'react'
 import Box from '@mui/material/Box'
 import throttle from 'lodash.throttle'
 
+const LINE_HEIGHT = 4
+const LETTERS_CONTAINER_WIDTH = 16
+
 const VideoTimeline = ({
   bufferedRegions, // array of [start, end] of unit frames, sorted by start
   existingRegions, // array of { letter, start, end } of unit frames
@@ -49,6 +52,14 @@ const VideoTimeline = ({
     setClickLineXPercent(xPercent)
   }
 
+  const existingRegionsContainerRef = useRef(null)
+  const scrollbarWidth = (() => {
+    const containerEl = existingRegionsContainerRef.current
+    if (!containerEl) return 0
+    return containerEl.offsetWidth - containerEl.clientWidth
+  })()
+  const TIMELINE_LEFT_INSET = scrollbarWidth + LETTERS_CONTAINER_WIDTH
+
   const [isDragging, setIsDragging] = useState(false)
   const dragToSeek = useCallback(
     throttle((event) => {
@@ -87,158 +98,200 @@ const VideoTimeline = ({
         overflowX: 'clip',
         overflowY: 'visible',
       }}
-      ref={containerRef}
-      onPointerMove={handlePointerMove}
-      onPointerLeave={() => setShowClickLine(false)}
     >
-      {/* Clickable Scrubber/Seeker */}
-      <Box sx={{ width: '100%', height: '100%' }} onClick={seekToPointerEvent} />
-      {showClickLine && (
-        <Box
-          sx={{
-            position: 'absolute',
-            top: 0,
-            left: `${clickLineXPercent}%`,
-            width: '2px',
-            height: '100%',
-            backgroundColor: 'secondary.main',
-            opacity: 0.4,
-            pointerEvents: 'none',
-          }}
-        />
-      )}
-
       {/* Existing Regions */}
-      {existingRegions.map((region) => {
-        const { letter, start, end } = region
-        const track = letterToTrackMap[letter]
-        return (
-          <Box
-            key={`${letter}-${start}-${end}`}
-            sx={{
-              position: 'absolute',
-              top: `calc(${track * 10}px + 6px)`,
-              left: `${(start / videoDuration) * 100}%`,
-              width: `${((end - start) / videoDuration) * 100}%`,
-              height: '4px',
-              backgroundColor: 'tertiary.main',
-            }}
-          />
-        )
-      })}
-
-      {/* Current Region */}
-      <Box
-        sx={{
-          position: 'absolute',
-          bottom: 'calc(5% + 4px)',
-          left: `${regionStartPosition}%`,
-          width: `calc(${regionEndPosition - regionStartPosition}%)`,
-          height: 'calc(90% - 4px)',
-          backgroundColor: 'tertiary.dark',
-          opacity: 0.1,
-          borderRadius: '4px',
-          visibility: regionStart == null || regionEnd == null ? 'hidden' : 'visible',
-        }}
-      />
-      <Box
-        sx={(theme) => ({
-          position: 'absolute',
-          bottom: 'calc(5% + 4px)',
-          left: `${regionStartPosition}%`,
-          width: '6px',
-          height: 'calc(90% - 4px)',
-          border: `2px solid ${theme.palette.tertiary.dark}`,
-          borderRight: 'none',
-          borderTopLeftRadius: '4px',
-          borderBottomLeftRadius: '4px',
-          visibility: regionStart == null ? 'hidden' : 'visible',
-        })}
-      />
-      <Box
-        sx={(theme) => ({
-          position: 'absolute',
-          bottom: 'calc(5% + 4px)',
-          left: `calc(${regionEndPosition}% - 4px)`,
-          width: '6px',
-          height: 'calc(90% - 4px)',
-          border: `2px solid ${theme.palette.tertiary.dark}`,
-          borderLeft: 'none',
-          borderTopRightRadius: '4px',
-          borderBottomRightRadius: '4px',
-          visibility: regionEnd == null ? 'hidden' : 'visible',
-        })}
-      />
-
-      {/* Buffer Bar */}
-      {bufferedRegions.map((region, index) => {
-        const [start, end] = region
-        return (
-          <Box
-            key={`${index}`}
-            sx={(theme) => ({
-              position: 'absolute',
-              bottom: 0,
-              left: `${(start / videoDuration) * 100}%`,
-              width: `${((end - start) / videoDuration) * 100}%`,
-              height: '4px',
-              backgroundColor: 'action.selected',
-              transition: `width ${theme.transitions.duration.shortest}ms ease-in-out`,
-            })}
-          />
-        )
-      })}
-
-      {/* Playhead */}
       <Box
         sx={{
           position: 'absolute',
           top: 0,
-          left: `calc(${playheadPosition}% - ${accountForPlayheadWidthNearRightEdge}px)`,
-          width: '2px',
+          width: '100%',
           height: '100%',
-          backgroundColor: 'secondary.main',
+          direction: 'rtl',
+          overflowY: 'scroll',
         }}
-      />
-      <Box
-        sx={(theme) => ({
-          position: 'absolute',
-          bottom: 0,
-          left: `calc(${playheadPosition}% - 5px - ${accountForPlayheadWidthNearRightEdge}px)`,
-          width: '0px',
-          height: '0px',
-          borderLeft: '6px solid transparent',
-          borderRight: '6px solid transparent',
-          borderBottom: `6px solid ${theme.palette.secondary.main}`,
+        ref={existingRegionsContainerRef}
+      >
+        <Box
+          sx={{
+            // This container is already pushed left by the scrollbar, so we only need to
+            // push it further left by the letters container width
+            width: `calc(100% - ${LETTERS_CONTAINER_WIDTH}px)`,
+            height: '100%',
+            marginLeft: `${LETTERS_CONTAINER_WIDTH}px`,
+            position: 'relative',
+          }}
+        >
+          {existingRegions.map((region) => {
+            const { letter, start, end } = region
+            const track = letterToTrackMap[letter]
+            return (
+              <Box
+                key={`${letter}-${start}-${end}`}
+                sx={{
+                  position: 'absolute',
+                  top: `calc(${track * 10}px + 6px)`,
+                  left: `${(start / videoDuration) * 100}%`,
+                  width: `${((end - start) / videoDuration) * 100}%`,
+                  height: `${LINE_HEIGHT}px`,
+                  backgroundColor: 'tertiary.main',
+                }}
+              />
+            )
+          })}
+        </Box>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: `calc(${existingLetters.length * 10}px + 6px)`,
+            width: `${LINE_HEIGHT}px`,
+            height: `${LINE_HEIGHT}px`,
+            backgroundColor: 'transparent',
+          }}
+        />
+      </Box>
 
-          '&:hover': {
-            width: '16px',
-            height: '16px',
-            border: `2px solid ${theme.palette.action.active}`,
-            borderRadius: '8px',
-            backgroundColor: theme.palette.secondary.main,
-            transform: 'translate(-2px, 5px)',
-            boxShadow: theme.shadows[4],
-          },
+      {/* Interactive Area */}
+      <Box
+        sx={{
+          width: `calc(100% - ${TIMELINE_LEFT_INSET}px)`,
+          height: '100%',
+          marginLeft: `${TIMELINE_LEFT_INSET}px`,
+          position: 'relative',
+        }}
+        ref={containerRef}
+        onPointerMove={handlePointerMove}
+        onPointerLeave={() => setShowClickLine(false)}
+      >
+        {/* Clickable Scrubber/Seeker */}
+        <Box sx={{ width: '100%', height: '100%' }} onClick={seekToPointerEvent} />
+        {showClickLine && (
+          <Box
+            sx={{
+              position: 'absolute',
+              top: 0,
+              left: `${clickLineXPercent}%`,
+              width: '2px',
+              height: '100%',
+              backgroundColor: 'secondary.main',
+              opacity: 0.4,
+              pointerEvents: 'none',
+            }}
+          />
+        )}
+
+        {/* Current Region */}
+        <Box
+          sx={{
+            position: 'absolute',
+            bottom: 'calc(5% + 4px)',
+            left: `${regionStartPosition}%`,
+            width: `calc(${regionEndPosition - regionStartPosition}%)`,
+            height: 'calc(90% - 4px)',
+            backgroundColor: 'tertiary.dark',
+            opacity: 0.1,
+            borderRadius: '4px',
+            visibility: regionStart == null || regionEnd == null ? 'hidden' : 'visible',
+          }}
+        />
+        <Box
+          sx={(theme) => ({
+            position: 'absolute',
+            bottom: 'calc(5% + 4px)',
+            left: `${regionStartPosition}%`,
+            width: '6px',
+            height: 'calc(90% - 4px)',
+            border: `2px solid ${theme.palette.tertiary.dark}`,
+            borderRight: 'none',
+            borderTopLeftRadius: '4px',
+            borderBottomLeftRadius: '4px',
+            visibility: regionStart == null ? 'hidden' : 'visible',
+          })}
+        />
+        <Box
+          sx={(theme) => ({
+            position: 'absolute',
+            bottom: 'calc(5% + 4px)',
+            left: `calc(${regionEndPosition}% - 4px)`,
+            width: '6px',
+            height: 'calc(90% - 4px)',
+            border: `2px solid ${theme.palette.tertiary.dark}`,
+            borderLeft: 'none',
+            borderTopRightRadius: '4px',
+            borderBottomRightRadius: '4px',
+            visibility: regionEnd == null ? 'hidden' : 'visible',
+          })}
+        />
+
+        {/* Buffer Bar */}
+        {bufferedRegions.map((region, index) => {
+          const [start, end] = region
+          return (
+            <Box
+              key={`${index}`}
+              sx={(theme) => ({
+                position: 'absolute',
+                bottom: 0,
+                left: `${(start / videoDuration) * 100}%`,
+                width: `${((end - start) / videoDuration) * 100}%`,
+                height: `${LINE_HEIGHT}px`,
+                backgroundColor: 'action.selected',
+                transition: `width ${theme.transitions.duration.shortest}ms ease-in-out`,
+              })}
+            />
+          )
         })}
-        onPointerDown={() => setIsDragging(true)}
-      />
-      {isDragging && (
+
+        {/* Playhead */}
+        <Box
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: `calc(${playheadPosition}% - ${accountForPlayheadWidthNearRightEdge}px)`,
+            width: '2px',
+            height: '100%',
+            backgroundColor: 'secondary.main',
+          }}
+        />
         <Box
           sx={(theme) => ({
             position: 'absolute',
             bottom: 0,
-            left: `${clickLineXPercent}%`,
-            width: '16px',
-            height: '16px',
-            border: `2px solid ${theme.palette.action.active}`,
-            borderRadius: '8px',
-            backgroundColor: theme.palette.secondary.main,
-            transform: 'translate(-7px, 5px)',
-            boxShadow: theme.shadows[4],
+            left: `calc(${playheadPosition}% - 5px - ${accountForPlayheadWidthNearRightEdge}px)`,
+            width: '0px',
+            height: '0px',
+            borderLeft: '6px solid transparent',
+            borderRight: '6px solid transparent',
+            borderBottom: `6px solid ${theme.palette.secondary.main}`,
+
+            '&:hover': {
+              width: '16px',
+              height: '16px',
+              border: `2px solid ${theme.palette.action.active}`,
+              borderRadius: '8px',
+              backgroundColor: theme.palette.secondary.main,
+              transform: 'translate(-2px, 5px)',
+              boxShadow: theme.shadows[4],
+            },
           })}
+          onPointerDown={() => setIsDragging(true)}
         />
-      )}
+        {isDragging && (
+          <Box
+            sx={(theme) => ({
+              position: 'absolute',
+              bottom: 0,
+              left: `${clickLineXPercent}%`,
+              width: '16px',
+              height: '16px',
+              border: `2px solid ${theme.palette.action.active}`,
+              borderRadius: '8px',
+              backgroundColor: theme.palette.secondary.main,
+              transform: 'translate(-7px, 5px)',
+              boxShadow: theme.shadows[4],
+            })}
+          />
+        )}
+      </Box>
     </Box>
   )
 }
