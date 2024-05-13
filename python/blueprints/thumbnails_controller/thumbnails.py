f40563acdc2757693528efe1dc36b38423404926
Ben Zenker <imbenzenker@gmail.com>
2024-05-12 18:59:12 -0600

    moving file write logic into service module

--- python/blueprints/thumbnails_controller/thumbnails.py
+++ python/blueprints/thumbnails_controller/thumbnails.py
@@ -1,27 +1,18 @@
-import os
 from flask import Blueprint, jsonify, request
 from urllib.parse import unquote_plus
 
-from settings.settings_service import SettingsService
-from settings.settings_enum import SettingsEnum
+from services.thumbnail_service import ThumbnailService
 
 bp = Blueprint('thumbnails', __name__)
-settings_service = SettingsService()
+thumbnail_service = ThumbnailService()
 
 
 @bp.route('/<filepath>', methods=['POST'])
 def save_thumbnail(filepath):
     try:
         blob = request.get_data()
-        thumbnail_dir_path = settings_service.get_setting(SettingsEnum.THUMBNAIL_DIR_PATH.value)
         filepath_decoded = unquote_plus(filepath)
-        final_file_path = os.path.join(thumbnail_dir_path, filepath_decoded)
-
-        final_file_dir = os.path.dirname(final_file_path)
-        os.makedirs(final_file_dir, exist_ok=True)
-        with open(final_file_path, 'wb') as aFile:
-            aFile.write(blob)
-
-        return jsonify(filepath_decoded), 200
+        thumbnail_service.write_thumbnail(blob, filepath_decoded)
+        return jsonify('success'), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
