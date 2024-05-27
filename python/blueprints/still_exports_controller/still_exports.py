1c0a6eccb6acd1de4ea3ef39238d1df3dfda1712
Matthew Crawford <mcrawford0126@gmail.com>
2024-05-27 11:54:31 -0400

    only create a still db entry if the still was successfully created

--- python/blueprints/still_exports_controller/still_exports.py
+++ python/blueprints/still_exports_controller/still_exports.py
@@ -3,7 +3,6 @@ from model.still_export_model import StillExportModel
 from services.still_export_service import StillExportService
 
 bp = Blueprint('still_exports', __name__)
-still_export_model = StillExportModel()
 still_frame_service = StillExportService()
 
 
@@ -11,8 +10,7 @@ still_frame_service = StillExportService()
 def create_still_export():
     payload = request.json
     try:
-        still_export_model.create_still_export(payload)
-        still_frame_service.extract_frame(payload['CatalogVideoId'], payload['FileLocation'], payload['FileName'], payload['FrameNumber'])
+        still_frame_service.create_still(payload)
         return jsonify({"message": "Still export created successfully"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
--- python/services/still_export_service.py
+++ python/services/still_export_service.py
@@ -1,29 +1,38 @@
 import subprocess
 import os
 import sys
+import time
 from model.video_model import VideoModel
+from model.still_export_model import StillExportModel
 from utils import file_path
 from settings.settings_enum import SettingsEnum
+from utils.prints import print_err, print_out
 
 
 class StillExportService:
 
     def __init__(self):
         self.video_model = VideoModel()
+        self.still_export_model = StillExportModel()
         current_dir = os.path.dirname(__file__)
         base_dir = os.path.abspath(os.path.join(current_dir, '..\\..'))
         self.ffmpeg_path = os.path.join(base_dir, 'ffmpeg', 'ffmpeg.exe')
 
-    def extract_frame(self, catalog_video_id, output_image_path, output_image_name, frame_number):
+    def create_still(self, payload):
         try:
+            catalog_video_id = payload['CatalogVideoId']
+            output_image_path = payload['FileLocation']
+            output_image_name = payload['FileName']
+            frame_number = payload['FrameNumber']
+            created_by = payload['CreatedBy']
+            created_date = payload['CreatedDate']
+
             original_video_folder_path = file_path.catalog_folder_path(catalog_video_id, SettingsEnum.BASE_FOLDER_OF_ORIGINAL_VIDEOS.value)
 
             catalog_video = self.video_model.get_video_by_id(catalog_video_id)
             original_video_name = catalog_video['OriginalFileName']
 
             original_video_file_path = str(os.path.join(original_video_folder_path, original_video_name))
-            sys.stderr.write('\n' + original_video_file_path + '\n')
-
             output_file_path = str(os.path.join(output_image_path, output_image_name))
 
             command = [
@@ -31,10 +40,22 @@ class StillExportService:
                 '-y',
                 '-i', original_video_file_path,
                 '-vf', f'select=eq(n\\,{frame_number})',
-                '-vframes', '1',
+                '-vframes:v', '1',
+                '-update', '1',
                 output_file_path
             ]
             subprocess.run(command, check=True)
-            sys.stderr.write(f"Frame extracted successfully and saved to {output_image_path}")
+            if os.path.isfile(output_file_path):
+                self.still_export_model.create_still_export({
+                    'CatalogVideoId': catalog_video_id,
+                    'FileName': output_image_name,
+                    'FileLocation': output_image_path,
+                    'CreatedBy': created_by,
+                    'CreatedDate': created_date
+                })
+                print_out(f"Frame extracted successfully and saved to {output_image_path}")
+            else:
+                raise Exception(f"Failed to extract frame from video: {original_video_name}")
         except Exception as e:
-            sys.stderr.write(f"An error occurred: {e}")
+            print_err(f"An error occurred: {e}")
+            raise e
