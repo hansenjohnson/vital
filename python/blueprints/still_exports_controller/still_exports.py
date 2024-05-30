f3865ce09dc19a36f035a74dceae627d861b02ae
Ben Zenker <imbenzenker@gmail.com>
2024-05-30 11:14:25 -0600

    eliminating references to stillframe

--- python/blueprints/still_exports_controller/still_exports.py
+++ python/blueprints/still_exports_controller/still_exports.py
@@ -3,14 +3,14 @@ from model.still_export_model import StillExportModel
 from services.still_export_service import StillExportService
 
 bp = Blueprint('still_exports', __name__)
-still_frame_service = StillExportService()
+still_export_service = StillExportService()
 
 
 @bp.route('', methods=['POST'], strict_slashes=False)
 def create_still_export():
     payload = request.json
     try:
-        still_frame_service.create_still(payload)
+        still_export_service.create_still(payload)
         return jsonify({"message": "Still export created successfully"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
--- ui/constants/settingKeys.js
+++ ui/constants/settingKeys.js
@@ -4,7 +4,7 @@ const SETTING_KEYS = {
   LINKAGE_FILE_PATH: 'linkage_file_path',
   SIGHTING_FILE_PATH: 'sighting_file_path',
   THUMBNAIL_DIR_PATH: 'thumbnail_dir_path',
-  STILLFRAME_DIR_NAME: 'stillframe_dir_path',
+  STILLEXPORT_DIR_NAME: 'stillexport_dir_path',
   BASE_FOLDER_OF_VIDEOS: 'base_folder_of_videos',
   FOLDER_OF_VIDEOS: 'folder_of_videos',
 }
