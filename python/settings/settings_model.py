f16d777cec13085a596fdd3131baa087c98fd12e
Matthew Crawford <mcrawford0126@gmail.com>
2024-07-04 12:41:11 -0400

    switch settings to settings.ini file

--- python/settings/settings_model.py
+++ python/settings/settings_model.py
@@ -1,12 +1,27 @@
-from sqlitedict import SqliteDict
+import configparser
+import os
 
 
 class SettingsModel:
     def __init__(self):
-        self.db = SqliteDict('./settings_controller.sqlite', autocommit=True)
+        self.config = configparser.ConfigParser()
+        print(os.getenv('APPDATA'))
+        self.file_path = os.path.join(os.getenv('APPDATA'), 'VITAL', 'settings.ini')
+        os.makedirs(os.path.dirname(self.file_path), exist_ok=True)
+        self.config.read(self.file_path)
 
     def get_setting(self, key):
-        return self.db.get(key)
+        if self.config.has_section('Settings'):
+            try:
+                return self.config.get('Settings', key)
+            except configparser.NoOptionError:
+                return ''
+        else:
+            return ''
 
     def set_setting(self, key, value):
-        self.db[key] = value
+        if not self.config.has_section('Settings'):
+            self.config.add_section('Settings')
+        self.config.set('Settings', key, value)
+        with open(self.file_path, 'w') as configfile:
+            self.config.write(configfile)
