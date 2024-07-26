6a851f40d8146dcf63862dd9e01192b70829f58f
Ben Zenker <imbenzenker@gmail.com>
2024-07-26 14:08:11 -0600

    connecting the api

--- ui/api/ingest.js
+++ ui/api/ingest.js
@@ -1,11 +1,8 @@
-// import { baseURL } from './config'
-// import { getJSON } from './fetchers'
+import { baseURL } from './config'
+import { getJSON } from './fetchers'
 
-// const countFiles = (sourceFolder) =>
-//   getJSON(`${baseURL}/ingest/count_files/${encodeURIComponent(sourceFolder)}`)
-
-const countFiles = () =>
-  new Promise((resolve) => setTimeout(() => resolve({ images: 765, videos: 28 }), 300))
+const countFiles = (sourceFolder) =>
+  getJSON(`${baseURL}/ingest/count_files/${encodeURIComponent(sourceFolder)}`)
 
 const parse = () => new Promise((resolve) => setTimeout(resolve, 500))
 const getCompressionOptions = () => new Promise((resolve) => setTimeout(resolve, 500))
