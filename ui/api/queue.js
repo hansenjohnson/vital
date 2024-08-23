296d0391aab476dec13e1b40f11d84365ca5769e
Ben Zenker <imbenzenker@gmail.com>
2024-08-23 12:12:08 -0600

    ui method for queue stats

--- ui/api/queue.js
+++ ui/api/queue.js
@@ -1,10 +1,16 @@
 import { baseURL } from './config'
-import { postJSON } from './fetchers'
+import { getJSON, postJSON } from './fetchers'
 
 const queueURL = `${baseURL}/queue`
 
 const executeNow = async () => postJSON(`${queueURL}/now`)
 
+const isRunning = async () => {
+  const res = await getJSON(`${queueURL}/status`)
+  return res.is_running
+}
+
 export default {
   executeNow,
+  isRunning,
 }
