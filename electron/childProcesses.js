c0272a2c6b26b5686640ef295c0b7fa0309b7802
Ben Zenker <imbenzenker@gmail.com>
2024-08-29 08:24:15 -0600

    reduce log noise

--- electron/childProcesses.js
+++ electron/childProcesses.js
@@ -4,18 +4,36 @@ import { app } from 'electron'
 import { is } from '@electron-toolkit/utils'
 import log from 'electron-log/main'
 
+const popularHTTPLogs = [
+  new RegExp('/queue/status'),
+  new RegExp('/ingest/job/\\d+'),
+  new RegExp('/ingest/job/\\d+/tasks'),
+]
+const TEN_MINUTES = 10 * 60 * 1000
+
 const logSpawnedServer = (server, infoLogger, errorLogger) => {
   server.stdout.on('data', (data) => {
     data = data.toString().trim()
     infoLogger(`Python stdout: ${data}`)
   })
 
+  const recentLines = {}
   server.stderr.on('data', (data) => {
     // NOTE: Flask logs end up here because Flask logs to stderr
     // Skip this log as we send it very frequently and reccuring
     if (data.includes('GET /settings/open_files')) return
     data = data.toString().trim()
     if (data.includes('HTTP')) {
+      // Silence popular log lines for 10 minutes
+      const popularRegex = popularHTTPLogs.find((regex) => regex.test(data))
+      if (popularRegex) {
+        if (popularRegex in recentLines) {
+          const timeDiff = new Date() - recentLines[popularRegex]
+          if (timeDiff < TEN_MINUTES) return
+        }
+        recentLines[popularRegex] = new Date()
+      }
+
       infoLogger(`Server: ${data}`)
     } else {
       errorLogger(`Python stderr: ${data}`)
