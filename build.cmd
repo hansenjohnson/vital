419deb85fc63b7fbdf9cfa8fb274cf0552551093
Ben Zenker <imbenzenker@gmail.com>
2024-09-01 10:26:11 -0600

    copies exif deps into prod build

--- build.cmd
+++ build.cmd
@@ -1,7 +1,12 @@
 pip install -r requirements.txt
+
 if not exist bin mkdir bin
 if not exist bin\resources mkdir bin\resources
+if not exist bin\resources\exiftool_files mkdir bin\resources\exiftool_files
+
 copy /Y python\resources\ bin\resources
+copy /Y python\resources\exiftool_files\ bin\resources\exiftool_files
+
 pyinstaller --onefile --distpath bin python/server.py
 
 npm run build:win
