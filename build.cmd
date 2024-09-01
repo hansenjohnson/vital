bdcdd8f40f3266e1b6a02d9e17723c0cd7b31f00
Ben Zenker <imbenzenker@gmail.com>
2024-09-01 10:31:05 -0600

    copying directory recusivley

--- build.cmd
+++ build.cmd
@@ -5,7 +5,7 @@ if not exist bin\resources mkdir bin\resources
 if not exist bin\resources\exiftool_files mkdir bin\resources\exiftool_files
 
 copy /Y python\resources\ bin\resources
-copy /Y python\resources\exiftool_files\ bin\resources\exiftool_files
+xcopy python\resources\exiftool_files\ bin\resources\exiftool_files /s /e /y
 
 pyinstaller --onefile --distpath bin python/server.py
 
